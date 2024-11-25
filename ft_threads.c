0ca9a73025ae5238d3922d60ed897f5b9c7348de
ManuCovr <mtmpfb@gmail.com>
2024-11-25 14:57:27 +0000

    threads more

--- ft_threads.c
+++ ft_threads.c
@@ -6,7 +6,7 @@
 /*   By: mde-maga <mde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:05:34 by mde-maga          #+#    #+#             */
-/*   Updated: 2024/11/21 16:07:13 by mde-maga         ###   ########.fr       */
+/*   Updated: 2024/11/25 14:57:09 by mde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@ void *is_it_dead(void *data)
 {
 	t_philo	*ph;
 
-	
+	ph = (t_philo *)data;
+	ft_usleep(ph->pa->rip + 1);
+	pthread_mutex_lock(&ph->pa->time_to_eat);
+	pthread_mutex_lock(&ph->pa->finish);
+	if(!check_death(ph, 0) && !ph->finish_please && ((time_of_day() - ph->eat_it_up) \
+														>= (long) (ph->pa->rip)));
+	{
+		pthread_mutex_unlock(&ph->pa->time_to_eat);
+		pthread_mutex_unlock(&ph->pa->finish);
+		pthread_mutex_lock(&ph->pa->write);
+		write_status()
+	}
 }
